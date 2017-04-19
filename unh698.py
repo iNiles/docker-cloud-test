293b93c454aebc88804d61b6e4c536ecf8c84515
iNiles <njamesdaly@gmail.com>
2017-04-19 00:42:05 -0400

    Update unh698.py

--- unh698.py
+++ unh698.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'If I get this server up it will be a miracle'
+    return 'If I get this server up it will be a miracle UNH698 Website' 
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
