c2cf377261d0b6dda9ed15467fb2cbbed577d35d
iNiles <njamesdaly@gmail.com>
2017-04-18 23:07:35 -0400

    Update unh698.py

--- unh698.py
+++ unh698.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def mainRoute():
     return render_template('index.html')
 
+@app.route('/computer-build')
+def computer():
+    return render_template('computer.html')
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5000)
-    
+    app.run(debug=True,host='0.0.0.0', port=8080)
