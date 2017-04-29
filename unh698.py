24e3e9740f615703b5b037dcf8ed2c39a92ba94e
iNiles <njamesdaly@gmail.com>
2017-04-29 16:45:02 -0400

    spaces

--- unh698.py
+++ unh698.py
@@ -4,8 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'If I get this server up it will be a miracle UNH698 Website' 
+
 @app.route('/secondpage')
 def hello_otherworld():
     return 'Oh my god poptarts'
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
