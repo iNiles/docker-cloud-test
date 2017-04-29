5be55916a795967673f7216254d7b74fb9281909
iNiles <njamesdaly@gmail.com>
2017-04-29 17:02:43 -0400

    Update unh698.py

--- unh698.py
+++ unh698.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'If I get this server up it will be a miracle UNH698 Website' 
+    return render_template('index.html")
 
 @app.route('/secondpage')
 def hello_otherworld():
-    return 'Oh my god poptarts'
-
+    return render_template('other.html")
+                           
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
