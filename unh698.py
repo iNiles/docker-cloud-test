bab21a31b796d4e8d4bc79a438a13ecaad5f3ecd
iNiles <njamesdaly@gmail.com>
2017-04-29 17:47:58 -0400

    '  not '

--- unh698.py
+++ unh698.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html")
+    return render_template('index.html')
 
 @app.route('/secondpage')
 def hello_otherworld():
-    return render_template('other.html")
+    return render_template('other.html')
                            
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
